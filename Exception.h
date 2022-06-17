6e0ad466c301fe977f74d4ca613cf41a1fa46c5b
AdiTsachGit <tsach.adi@gmail.com>
2022-06-17 11:34:40 +0300

    f

--- Exception.h
+++ Exception.h
@@ -10,19 +10,19 @@ using std::cout;
 class Exception : public std::exception {
 public:
 
-    /** Constructor (C strings).
-     *  @param message C-style string error message.
-     *                 The string contents are copied upon construction.
-     *                 Hence, responsibility for deleting the char* lies
-     *                 with the caller.
-     */
-    explicit Exception(const char* message) : msg_(message) {}
+//    /** Constructor (C strings).
+//     *  @param message C-style string error message.
+//     *                 The string contents are copied upon construction.
+//     *                 Hence, responsibility for deleting the char* lies
+//     *                 with the caller.
+//     */
+    Exception(){}
 
 
-    /** Constructor (C++ STL strings).
-     *  @param message The error message.
-     */
-    explicit Exception(const std::string& message) : msg_(message) {}
+//    /** Constructor (C++ STL strings).
+//     *  @param message The error message.
+//     */
+//    explicit Exception(const std::string& message) : msg_(message) {}
 
 
     bool operator==(const Exception& other) const;
@@ -31,10 +31,21 @@ public:
     /** Destructor.
     * Virtual to allow for subclassing.
     */
-    virtual ~Exception() noexcept {}
+    ~Exception() noexcept {}
+
     class DeckFileNotFound{};
-    class DeckFileFormatError(int line){};
     class DeckFileInvalidSize{};
+
+    class DeckFileFormatError{
+    public:
+        DeckFileFormatError(const int line) : m_lineError(line) {}
+        int getLine() const;
+    private:
+        int m_lineError;
+    };
+
+
+   // DeckFileFormatError(int line) : Exception(int line){};
 
 
     /** Returns a pointer to the (constant) error description.
@@ -43,12 +54,11 @@ public:
      *          not attempt to free the memory.
      */
      const char* what() const noexcept override {
-        return msg_.c_str();
-        if (*this == Exception::DeckFileInvalidSize()){
+        if (this == Exception::DeckFileInvalidSize()){
             cout << "Deck File Error: Deck size is invalid" << std::endl;
         }
         else if (this == DeckFileFormatError()){
-            cout << "Deck File Error: File format error in line" << lineNumber << std::endl;
+            cout << "Deck File Error: File format error in line" << this->getLine() << std::endl;
         }
         else if (this == DeckFileNotFound()){
             cout << "Deck File Error: File not found" << std::endl;
@@ -58,14 +68,17 @@ public:
         }
     }
 
-protected:
-    /** Error message.
-     */
-    std::string msg_;
+//protected:
+//    /** Line of error.
+//     */
+//    int m_lineError;
 };
 
 
-
+int Exception::DeckFileFormatError::getLine() const
+{
+    return m_lineError;
+}
 
 
 
